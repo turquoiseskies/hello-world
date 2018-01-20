70a7d779ab9cade44cc11fac7811feda73bda3c2
gavin samuel <gavin.s999@googlemail.com>
2018-01-20 21:06:38 +0000

    Added hello test

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+
+public class Hello {
+
+    String getHello(){
+        return "Hello";
+    }
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloTest {
+
+    @Test
+    public void testHello() throws Exception {
+
+        Hello hello = new Hello();
+        String helloResult = hello.getHello();
+        assertEquals(helloResult, "Hello");
+    }
+}
