8439927c5b797e9cb47b10dd8d8771c69c9cef82
gavin samuel <gavin.s999@googlemail.com>
2018-01-20 21:56:58 +0000

    changed getHello to return a constant variable

--- src/main/java/com/turquoiseskies/hello/Hello.java
+++ src/main/java/com/turquoiseskies/hello/Hello.java
@@ -2,7 +2,10 @@ package com.turquoiseskies.hello;
 
 public class Hello {
 
+
+    public static final String HELLO = "Hello";
+
     String getHello(){
-        return "Hello";
+        return HELLO;
     }
 }
